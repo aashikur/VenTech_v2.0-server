7ad8c5349596ce45e6ef00c09ee6b8fc09bef18e
aashikur <mdaashikur@gmail.com>
2025-09-30 23:33:20 +0600

    .

--- index.js
+++ index.js
@@ -996,6 +996,24 @@ app.get("/api/v1/get-request-byID/:id", async( req, res) => {
 
 
 
+
+
+
+
+
+
+
+
+
+
+// ----------------- CORS -----------------
+app.use(cors());
+
+// ----------------- Body Parser -----------------
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
+
 // ----------------- Error Handler -----------------
 app.use((err, _req, res, _next) => {
   console.error("Unhandled error:", err);
